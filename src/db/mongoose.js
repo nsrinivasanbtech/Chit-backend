2fa8c46e6bb4025a3a38245654feded6ae9baab8
nsrinivasanbtech <48716788+nsrinivasanbtech@users.noreply.github.com>
2020-12-02 21:23:54 +0530

    mongo db name change

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://srini:Srini123@cluster0.bcfor.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://srini:Srini123@cluster0.bcfor.mongodb.net/Chit?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
